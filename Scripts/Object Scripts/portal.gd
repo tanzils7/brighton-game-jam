a14738cc129fd9a7a333550189b589078e763a55
Ali Yousuf Sheikh <aliys31415@gmail.com>
2026-03-01 13:12:32 +0000

    diudfhdfjhdf

--- Scripts/Object Scripts/portal.gd
+++ Scripts/Object Scripts/portal.gd
@@ -1,9 +1,10 @@
 extends Area2D
 
+@onready var animation = $AnimatedSprite2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	animation.play("swirl")
 
 
 
