a9d37331c07b6ee0a8317b9906d77167e0d0d46f
Ali Yousuf Sheikh <aliys31415@gmail.com>
2026-03-01 13:06:58 +0000

    dddd

---
+++ Scripts/Object Scripts/portal.gd
@@ -0,0 +1,11 @@
+extends Area2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+
+func _on_body_entered(body: Node2D) -> void:
+	print("yay")
