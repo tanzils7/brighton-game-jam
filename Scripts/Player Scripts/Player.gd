732cbbddf8a69caa5c9247f0047a8edab4766741
tanzils7 <tanzils213@gmail.com>
2026-03-01 11:57:25 +0000

    Added an attack for player
    tufffff diddyblud

--- Scripts/Player Scripts/Player.gd
+++ Scripts/Player Scripts/Player.gd
@@ -5,6 +5,7 @@ extends CharacterBody2D
 @onready var meleeHitbox = $MeleePivot/MeleeHitbox
 @onready var swordVFX = $MeleePivot/SwordVFX
 @onready var swingPivot = $MeleePivot 
+@onready var swordSFX = $"SFX Manager/swordSFX"
 
 const meleeCooldown: float = 0.5
 const attackLockTime: float = 0.12
@@ -40,34 +41,52 @@ func meleeAttack():
 	swingPivot.rotation = swingStartAngle
 	meleeHitbox.monitoring = true
 	swordVFX.visible = true
+	swordSFX.play();
 
-func updateTimers(delta:float):
+func updateTimers(delta: float):
+	# COOLDOWN TICK
+	if meleeTimer > 0:
+		meleeTimer -= delta
+		if meleeTimer < 0:
+			meleeTimer = 0
+
 	if attackLockTimer > 0:
 		attackLockTimer -= delta
 		if attackLockTimer <= 0:
 			isAttacking = false
-	
+
 	# HANDLE SWING ARC USING PIVOT
 	if attackActiveTimer > 0:
 		attackActiveTimer -= delta
-		
+
 		var t = 1.0 - (attackActiveTimer / attackActiveTime)
 		t = clamp(t, 0.0, 1.0)
-		
+
 		swingPivot.rotation = lerp(swingStartAngle, swingEndAngle, t)
-		
+
 		if attackActiveTimer <= 0:
 			meleeHitbox.monitoring = false
 			swordVFX.visible = false
 
+func _ready() -> void:
+	swordVFX.visible = false
+	meleeHitbox.monitoring = false
+
 func _physics_process(delta: float) -> void:
 	var inputDir: Vector2 = Input.get_vector("Left","Right","Up","Down")
+	if inputDir != Vector2.ZERO:
+		facingDir = inputDir.normalized()
 	
 	updateTimers(delta)
 	
 	if Input.is_action_just_pressed("Attack"):
 		meleeAttack()
 	
+	if Input.is_action_just_pressed("Reset"):
+		get_tree().reload_current_scene()
+	
 	velocity = inputDir * speed
+	
+	
 	
 	move_and_slide()
