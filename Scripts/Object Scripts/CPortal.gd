206dfe4ff99429035577f939d127ca31141cb563
Ali Yousuf Sheikh <aliys31415@gmail.com>
2026-03-01 13:45:17 +0000

    PortalRoomSwITCHsolved
    BASICALLY IT ALL WORKS NOW

--- Scripts/Object Scripts/CPortal.gd
+++ Scripts/Object Scripts/CPortal.gd
@@ -5,8 +5,12 @@ extends Area2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	animation.play("swirl")
+	
 
 
 
 func _on_body_entered(body: Node2D) -> void:
-	print("yay")
+	if body is CharacterBody2D:
+		Global.next_spawn_position = Vector2(200, 150)
+		get_tree().change_scene_to_file("res://Scenes/circuits_sub_room.tscn")
+		
---
+++ Scripts/global.gd
@@ -0,0 +1,4 @@
+extends Node
+
+var next_spawn_position: Vector2 = Vector2.ZERO
+var spawn_name: String = ""
